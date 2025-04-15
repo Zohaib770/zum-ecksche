4015a5bd8fe833b329f5a05d15d5498fe53c2cbe
Zohaib Khalid <kzohaib77@gmail.com>
2025-04-15 22:26:02 +0500

    add cartContext

---
+++ frontend/src/context/CartContext.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, useContext, useState } from "react";
+import { CartItem } from "../types/Interfaces";
+
+type CartContextType = {
+  items: CartItem[];
+  addToCart: (item: CartItem) => void;
+  removeFromCart: (index: number) => void;
+  clearCart: () => void;
+};
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) throw new Error("useCart must be used within CartProvider");
+  return context;
+};
+
+export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [items, setItems] = useState<CartItem[]>([]);
+
+  const addToCart = (item: CartItem) => {
+    setItems((prev) => [...prev, item]);
+  };
+
+  const removeFromCart = (index: number) => {
+    setItems((prev) => prev.filter((_, i) => i !== index));
+  };
+
+  const clearCart = () => {
+    setItems([]);
+  };
+
+  return (
+    <CartContext.Provider value={{ items, addToCart, removeFromCart, clearCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
