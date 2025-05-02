55b9154be94bb21d6c44d1519671aadcfb485566
Zohaib Khalid <kzohaib77@gmail.com>
2025-05-02 15:49:29 +0500

    add formate

--- frontend/src/components/FoodList.tsx
+++ frontend/src/components/FoodList.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState, useCallback } from 'react';
 import { Food, Option } from '../types/Interfaces';
 import { useNavigate } from "react-router-dom";
 import { useFoodContext } from "../context/FoodContext";
+import { convertPriceFromDotToComma } from '../utils/helpFunctions';
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 
@@ -197,69 +198,97 @@ const FoodList = () => {
                 className="flex justify-between items-center p-4 bg-gray-50 cursor-pointer hover:bg-gray-100 transition"
                 onClick={() => toggleCategory(catId)}
               >
-                <div className="flex items-center">
-                  {cat.imageUrl && (
-                    <img
-                      src={`${BACKEND_URL}/${cat.imageUrl}`}
-                      alt={cat.name}
-                      className="w-10 h-10 object-cover rounded mr-3"
-                    />
-                  )}
-                  <h3 className="font-medium">{cat.name}</h3>
-                </div>
-                <svg
-                  className={`w-5 h-5 text-gray-500 transform transition-transform ${expandedCategories[catId] ? 'rotate-180' : ''
-                    }`}
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
-                </svg>
-              </div>
-
-              {expandedCategories[catId] && (
-                <div className="p-4">
-                  <div className="space-y-3">
-                    {filteredFoods.map((food: Food) => (
-                      <div key={String(food._id)} className="pb-3 border-b last:border-0">
-                        <div className="flex justify-between">
-                          <div>
-                            <h4 className="font-medium">
-                              {food.name} <span className="font-medium">{food.price}</span>
-                            </h4>
-                            {food.description && (
-                              <p className="text-sm text-gray-600">{food.description}</p>
-                            )}
-                          </div>
-                          <button
-                            type="button"
-                            onClick={() => addFoodItem(food)}
-                            className="w-7 h-7 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
-                          >
-                            +
-                          </button>
-                        </div>
-                        {food.options?.map((option: Option, i) => (
-                          <div key={i} className="mt-1 text-sm">
-                            <span className="text-gray-600">{option.name}: </span>
-                            {option.values?.map((val, j) => (
-                              <span key={j} className="mr-2">
-                                {val.value} {val.price !== '' && `(${val.price})`}
-                              </span>
-                            ))}
-                          </div>
-                        ))}
-                      </div>
-                    ))}
+                <div className="flex items-start flex-col">
+                  <div className="flex items-center mb-1">
+                    {cat.imageUrl && (
+                      <img
+                        src={`${BACKEND_URL}/${cat.imageUrl}`}
+                        alt={cat.name}
+                        className="w-10 h-10 object-cover rounded mr-3"
+                      />
+                    )}
+                    <h3 className="font-medium text-lg">{cat.name}</h3>
                   </div>
-                </div>
-              )}
+
+                  {cat.description && (
+                    <div className="text-sm text-gray-600 mb-1">
+                      {cat.description}
+                    </div>
+                  )}
+
+                  {cat.options && cat.options.length > 0 && (
+                    <div className="text-sm text-gray-500">
+                      {cat.options.map((option, i) => (
+                        <div key={i} className="mt-1">
+                          <span className="font-medium">{option.name}:</span>
+                          {option.values?.map((val, j) => (
+                            <span key={j} className="ml-2">
+                              {val.value}
+                              {val.price && ` (${convertPriceFromDotToComma(parseFloat(val.price))}€)`}
+                              {j < (option.values?.length || 0) - 1 ? ',' : ''}
+                            </span>
+                          ))}
+                        </div>
+                      ))}
+                    </div>
+                  )}
+
+              </div>
+              <svg
+                className={`w-5 h-5 text-gray-500 transform transition-transform ${expandedCategories[catId] ? 'rotate-180' : ''
+                  }`}
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+              </svg>
             </div>
-          );
+
+              {
+            expandedCategories[catId] && (
+              <div className="p-4">
+                <div className="space-y-3">
+                  {filteredFoods.map((food: Food) => (
+                    <div key={String(food._id)} className="pb-3 border-b last:border-0">
+                      <div className="flex justify-between">
+                        <div>
+                          <h4 className="font-medium">
+                            {food.name} <span className="font-medium">{food.price}</span>
+                          </h4>
+                          {food.description && (
+                            <p className="text-sm text-gray-600">{food.description}</p>
+                          )}
+                        </div>
+                        <button
+                          type="button"
+                          onClick={() => addFoodItem(food)}
+                          className="w-7 h-7 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+                        >
+                          +
+                        </button>
+                      </div>
+                      {food.options?.map((option: Option, i) => (
+                        <div key={i} className="mt-1 text-sm">
+                          <span className="text-gray-600">{option.name}: </span>
+                          {option.values?.map((val, j) => (
+                            <span key={j} className="mr-2">
+                              {val.value} {val.price !== '' && `(${val.price})`}
+                            </span>
+                          ))}
+                        </div>
+                      ))}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )
+          }
+            </div>
+      );
         })}
-      </div>
     </div>
+    </div >
   );
 };
 
