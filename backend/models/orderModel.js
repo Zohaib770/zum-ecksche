581e98419ac611f533cffe6f97e81de2b963527e
Zohaib Khalid <kzohaib77@gmail.com>
2025-05-03 22:20:45 +0500

    add new attributes

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -16,25 +16,32 @@ const PersonalDetailSchema = new mongoose.Schema({
 }, { _id: false });
 
 const cartItemSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    quantity: { type: Number },
-    price: { type: String, required: true },
-    options: [optionSchema],
-    comment: { type: String },
-  });
+  name: { type: String, required: true },
+  quantity: { type: Number },
+  price: { type: String, required: true },
+  options: [optionSchema],
+  comment: { type: String },
+});
 
 const OrderSchema = new mongoose.Schema({
   cartItem: [cartItemSchema],
   personalDetail: PersonalDetailSchema,
+  deliveryAddress: {
+    type: DeliveryAddressSchema, required: function () {
+      return this.orderType === 'delivery';
+    }
+  },
+  price: { type: String },
   orderType: { type: String, enum: ['delivery', 'pickup'], required: true },
   paymentMethod: { type: String, enum: ['cash', 'online'], required: true },
-  deliveryAddress: { type: DeliveryAddressSchema, required: function () {
-    return this.orderType === 'delivery';
-  }},
-  price: {type: String},
+  onlinePaymentMethod: { type: String, enum: ['paypal', 'giro'], required: false },
+  payPalOrderId: { type: String, default: '' },
+  paypalTransactionId: { type: String, default: '' },
   status: { type: String, default: 'new' },
   createdAt: { type: String, default: () => new Date().toISOString() }
 });
 
 const Order = mongoose.model('Order', OrderSchema);
 module.exports = Order;
+
+
