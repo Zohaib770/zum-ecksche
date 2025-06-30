c0fb993e5976ee39a228b62e2b98e6d9a371e37e
root <root@srv835886.hstgr.cloud>
2025-06-30 08:43:41 +0000

    Add postcss.config.js and update package.json

---
+++ frontend/postcss.config.js
@@ -0,0 +1,12 @@
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+/** @type {import('postcss-load-config').Config} */
+export default {
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+  ],
+};
+
+
